dc04f487ef5aea5cce2adf6e092576dc93607cb2
Veton Fetai <vetonfetai1@gmail.com>
2024-03-13 10:17:54 +0100

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,60 @@
-import { useState } from 'react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Book from './book.jsx';
 
 function App() {
-  const [books, setBooks] = useState(null);
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const apiURL = 'https://www.anapioficeandfire.com/api/books?pageSize=30';
-function fetchBooks() {
-  fetch(apiURL)
-  .then(resp => resp.json())
-  .then(data => {
-    setBooks(data);
-  })
 
-}
+  const fetchBooks = async () => {
+    try {
+      setLoading(true); 
+      const response = await fetch(apiURL);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      setBooks(data);
+      setError(null);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+      setError('Error fetching books. Please try again later.');
+    } finally {
+      setLoading(false); 
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchBooks();
+    };
+
+    fetchData();
+  }, []); 
+
   return (
     <div className="App">
       <h1>Game of Thrones Books</h1>
       <h2>Fetch a list from an API and display it</h2>
 
       <div>
-        <button className="fetch-button" onClick={fetchBooks}>Fetch Data</button>
+        <button className="fetch-button" onClick={fetchBooks} disabled={loading}>
+          Fetch Data
+        </button>
         <br />
       </div>
 
       <div className="books">
         {books &&
-          books.map((book, index) => {
-            const authors = book.authors.join(", ");
-
-            return (
-            <div className="book" key={index}>
-            <h3>Book {index + 1}</h3>
-            <h2>{book.name}</h2>
-
-            <div className="info">
-            <p>{authors}</p>
-            <p>{book.numberOfPages} pages</p>
-            <p>{book.country}</p>
-            <p>{book.released}</p>
-                </div>
-              </div>
-            );
-          })}
+          books.map((book, index) => (
+            <Book key={index} book={book} index={index} /> 
+          ))}
       </div>
     </div>
   );
 }
 
-
-export default App
+export default App;
